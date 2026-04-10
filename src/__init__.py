11420e4acf96554f2aab946cd8c12644a4aefd87
Dingqi Nai <98136620+naidingqi@users.noreply.github.com>
2026-04-10 18:30:06 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from .constants import *
 from .utils import set_seed, get_device
 from .data_generation import make_profile, generate_runs, odefun, generate_synthetic_data
-from .models import CrystallizationRHS, PIRNN
+from .models import PBM, PIRNN
 from .losses import lossX, lossG, lossSmooth
 from .training import train_sweep, evaluate_model, calculate_rhs_error
 from .evaluation import odefun_pred, reevaluate_params_on_test
